5386f8eba3679dc8668ec617e8fecae48bc1af20
Dipen505 <dipenchakma505@gmail.com>
2021-03-10 11:51:07 +0600

    function name changed

--- largestNumber.js
+++ largestNumber.js
@@ -1,6 +1,7 @@
-function call(num) {
+function largestNumber(num) {
     var number = 0;
-    for (var i = 0; i<=num.length; i++) {
+    var length = num.length;
+    for (var i = 0; i<=length; i++) {
         var result = array[i];
         if (number<result) {
             number = result;
@@ -9,7 +10,7 @@ function call(num) {
     return number;
 }
 var array = [23, 4, 54, 23, 574, 56, 76];
-var output = call(array);
+var output = largestNumber(array);
 console.log(output);
 
 
